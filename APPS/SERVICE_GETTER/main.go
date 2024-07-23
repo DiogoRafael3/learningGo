98b9f86cb2852a262da236f62f8f7cbf70c7aad5
DiogoRafael3 <diogoraf8@gmail.com>
2024-07-23 17:06:56 +0100

    removed messages from main in apps/service_getter and restructured packaging for new classes

--- APPS/SERVICE_GETTER/main.go
+++ APPS/SERVICE_GETTER/main.go
@@ -8,11 +8,6 @@ import (
 
 func main() {
 
-	//to run this application either run
-	//go run main.go ip --<COMMAND> <WEB_ADRESS>
-	//or
-	// ./serv_get ip --<COMMAND> <WEB_ADRESS>
-	//ip being the name of the command
 	application := app.Generate()
 
 	if err := application.Run(os.Args); err != nil { //this function allows us to run the application while getting the args from the command line, it outputs an error so we can treat it immediatly
